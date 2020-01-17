f9c0c1166fabe43caafba15d392006f7ea1fc2da
Martin Furuskog <martin.furuskog@uhr.se>
2020-01-17 14:55:29 +0100

    Initial commit

---
+++ recipeModel.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+// Setup schema
+var recipeSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String        
+    },
+    url: {
+        type: String,
+        required: true
+    },    
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Recipe = module.exports = mongoose.model('recipe', recipeSchema);
+module.exports.get = function (callback, limit) {
+    Recipe.find(callback).limit(limit);
+}
