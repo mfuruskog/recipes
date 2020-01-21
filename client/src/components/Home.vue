f020523199f4729a3ef86f81dd0c4a05a47ba96e
Martin Furuskog <martin.furuskog@uhr.se>
2020-01-21 10:53:47 +0100

    Removed some bootstrapped code and added RecipeList component

---
+++ client/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+ <recipe-list></recipe-list>
+</template>
+
+<script>
+import RecipeList from './RecipeList.vue'
+export default {
+  name: 'Home',
+  components: {
+    RecipeList
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/RecipeList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li>Item1</li>
+    <li>Item2</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "RecipeList"
+};
+</script>
+
+<style scoped>
+</style>
