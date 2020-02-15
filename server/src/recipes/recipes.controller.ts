fc34bfa621f71a44a2b1c166467ef76d77ede42f
Martin Furuskog <martin.furuskog@hotmail.com>
2020-02-15 16:40:09 +0100

    Recipe controller and dtos

--- server/src/recipes/recipes.controller.ts
+++ server/src/recipes/recipes.controller.ts
@@ -1,9 +1,32 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post, Param, Body, Put, Delete } from '@nestjs/common'
+import { CreateRecipeDto } from './create-recipe.dto'
+import { UpdateRecipeDto } from './update-recipe.dto'
 
 @Controller('recipes')
 export class RecipesController {
+
+    @Post()
+    async create(@Body() createRecipeDto: CreateRecipeDto): Promise<string> {
+        return 'Added!'
+    }
+
     @Get()
-    findAll(): string {
-        return 'This is it'
+    async findAll(): Promise<string> {
+        return 'This is it!'
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<string> {        
+        return `Fetched ${id}!`
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto): Promise<string> {
+        return 'Updated'
+    }
+    
+    @Delete(':id')
+    async Delete(@Param('id') id: string): Promise<string> {
+        return 'Deleted'
     }
 }
