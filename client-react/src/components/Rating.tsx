96f29d8b415ab78886614596845b9f43e6b210e4
mafu <mafu@netlight.com>
2020-06-25 08:41:03 +0200

    Changed star in rating to styled component

--- client-react/src/components/Rating.tsx
+++ client-react/src/components/Rating.tsx
@@ -1,20 +1,20 @@
 /** @jsx jsx */
 
 import { jsx } from '@emotion/core';
+import tw, { styled } from 'twin.macro';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
 
+const StarIcon = styled(FontAwesomeIcon)`
+  ${tw`text-yellow-600`}
+`;
 const Rating: React.FC<{ rating: number }> = ({ rating }) => {
   return (
     <div>
       {[...Array(5)].map((e, i) => (
-        <FontAwesomeIcon
-          tw="text-yellow-600"
-          icon={i < rating ? faStarSolid : faStarRegular}
-          key={i}
-        />
+        <StarIcon icon={i < rating ? faStarSolid : faStarRegular} key={i} />
       ))}
     </div>
   );
