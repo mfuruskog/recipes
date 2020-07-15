c80920605c9ff029356e2e81942fd806f60697ab
mafu <mafu@netlight.com>
2020-07-15 21:07:13 +0200

    Dockerize server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthzModule } from './authz/authz.module';
 
 @Module({
-  imports: [RecipesModule, MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost/recipes', { useNewUrlParser: true}), AuthzModule]
+  imports: [
+    RecipesModule,
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost/recipes',
+      { useNewUrlParser: true },
+    ),
+    AuthzModule,
+  ],
 })
 export class AppModule {}
